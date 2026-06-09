8da8007e69d616f8bceba011c3a6a8854993188b
mayank-141106 <mayanklande125@gmail.com>
2026-06-09 17:51:54 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -14,6 +14,7 @@ app.add_middleware(
         "http://localhost:5500",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5500",
+        "https://cig-project.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
